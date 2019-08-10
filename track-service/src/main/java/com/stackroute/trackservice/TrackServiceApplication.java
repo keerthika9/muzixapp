7d05392864f534288f7dc46d16bf77bf243550a3
keerthika <atmakurikeerthika@gmail.com>
2019-08-10 18:18:09 +0530

    adding dockerised muzix app

---
+++ track-service/src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
@@ -0,0 +1,19 @@
+package com.stackroute.trackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TrackServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TrackServiceApplication.class, args);
+  }
+
+}
+
+
